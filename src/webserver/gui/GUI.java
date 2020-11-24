ac18be666f5230ec353897bb3e206a29c212db9a
AlexBosca <alex_241998@yahoo.com>
2020-11-24 20:01:34 +0200

    Added control panel

--- src/webserver/gui/GUI.java
+++ src/webserver/gui/GUI.java
@@ -1,11 +1,13 @@
 package webserver.gui;
 
+import java.awt.Dimension;
 import java.awt.Font;
 //import java.awt.Color;
 import java.awt.GridLayout;
 
 import javax.swing.BorderFactory;
-//import javax.swing.JButton;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -47,9 +49,13 @@ public class GUI extends JFrame {
 		control = 		new JPanel();
 		configuration = new JPanel();
 		
+		/**
+		 * 	Setting informations panel
+		 */
+		
 		//info.setBackground(Color.WHITE);
 		info.setBorder(BorderFactory.createTitledBorder(null, "WebServer info", TitledBorder.LEFT, TitledBorder.TOP));
-		info.setLayout(new GridLayout(3, 2, 0, 0));
+		info.setLayout(new GridLayout(3, 2));
 		JLabel notRunning1 = new JLabel("not running");
 		notRunning1.setHorizontalAlignment(SwingConstants.RIGHT);
 		notRunning1.setFont(notRunning1.getFont().deriveFont(notRunning1.getFont().getStyle() & ~Font.BOLD));
@@ -78,11 +84,34 @@ public class GUI extends JFrame {
 		info.add(notRunning2);
 		info.add(port);		
 		info.add(notRunning3);
-		//control.setBackground(Color.WHITE);
-		control.setBorder(BorderFactory.createTitledBorder(null, "WebServer control", TitledBorder.LEFT, TitledBorder.TOP));
 		
 		//configuration.setBackground(Color.WHITE);
 		configuration.setBorder(BorderFactory.createTitledBorder(null, "WebServer configuration", TitledBorder.LEFT, TitledBorder.TOP));
+		
+		
+		
+		/**
+		 * 	Setting control panel
+		 */
+		
+		//control.setBackground(Color.WHITE);
+		control.setBorder(BorderFactory.createTitledBorder(null, "WebServer control", TitledBorder.LEFT, TitledBorder.TOP));
+		control.setLayout(new GridLayout(2, 1));
+		
+		JButton button = new JButton("Start server");
+		button.setPreferredSize(new Dimension(1, 1));
+		
+		JCheckBox maintenanceMode = new JCheckBox("Switch to maintenance mode");
+		maintenanceMode.setEnabled(false);
+		
+		control.add(button);
+		control.add(maintenanceMode);
+		
+		
+		/**
+		 * 	Setting configuration panel
+		 */
+		
 	}
 
 	public static void main(String[] args) {
