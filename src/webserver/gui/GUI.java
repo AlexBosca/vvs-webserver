ee6f0390fd95ae8f19b1997b3d3a00d62928159d
AlexBosca <alex_241998@yahoo.com>
2020-11-25 13:38:05 +0200

    Added configuration panel

--- src/webserver/gui/GUI.java
+++ src/webserver/gui/GUI.java
@@ -1,6 +1,10 @@
 package webserver.gui;
 
+import webserver.WebServer;
+
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
 //import java.awt.Color;
 import java.awt.GridLayout;
@@ -11,6 +15,7 @@ import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.border.TitledBorder;
 
@@ -20,24 +25,41 @@ public class GUI extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private static final String title = "VVS Web Server";
 	
 	private JPanel info, control, configuration;
 	private JPanel upperSection;
 	
+	private JLabel notRunning1;
+	private JLabel notRunning2;
+	private JLabel notRunning3;
+	
+	private JButton startServer;
+	private JCheckBox maintenanceMode;
+	
+	private JTextField tfPort;
+	private JTextField tfRootDir;
+	private JTextField tfMaintananceDir;
+	
+	private JButton browseRoot;
+	private JButton browseMaintenance;
+	private JButton ok;
+	private JButton cancel;
+	
 	public GUI() {
-		super("WebServer GUI");
+		super(title + " - " + "[" + WebServer.state + "]");
 		
 		setupPanels();
 		
 		this.setSize(550, 300);
 		this.setVisible(true);
+		this.setResizable(false);
 		this.setLocationRelativeTo(null);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		upperSection = new JPanel(new GridLayout(1, 2, 10, 0));
 		upperSection.add(info);
 		upperSection.add(control);
-		//upperSection.setBackground(Color.WHITE);
 		
 		this.setLayout(new GridLayout(2, 1, 0, 10));
 		this.add(upperSection);
@@ -53,18 +75,17 @@ public class GUI extends JFrame {
 		 * 	Setting informations panel
 		 */
 		
-		//info.setBackground(Color.WHITE);
 		info.setBorder(BorderFactory.createTitledBorder(null, "WebServer info", TitledBorder.LEFT, TitledBorder.TOP));
 		info.setLayout(new GridLayout(3, 2));
-		JLabel notRunning1 = new JLabel("not running");
+		notRunning1 = new JLabel("not running");
 		notRunning1.setHorizontalAlignment(SwingConstants.RIGHT);
 		notRunning1.setFont(notRunning1.getFont().deriveFont(notRunning1.getFont().getStyle() & ~Font.BOLD));
 		
-		JLabel notRunning2 = new JLabel("not running");
+		notRunning2 = new JLabel("not running");
 		notRunning2.setHorizontalAlignment(SwingConstants.RIGHT);
 		notRunning2.setFont(notRunning2.getFont().deriveFont(notRunning2.getFont().getStyle() & ~Font.BOLD));
 		
-		JLabel notRunning3 = new JLabel("not running");
+		notRunning3 = new JLabel("not running");
 		notRunning3.setHorizontalAlignment(SwingConstants.RIGHT);
 		notRunning3.setFont(notRunning3.getFont().deriveFont(notRunning3.getFont().getStyle() & ~Font.BOLD));
 		
@@ -85,26 +106,22 @@ public class GUI extends JFrame {
 		info.add(port);		
 		info.add(notRunning3);
 		
-		//configuration.setBackground(Color.WHITE);
-		configuration.setBorder(BorderFactory.createTitledBorder(null, "WebServer configuration", TitledBorder.LEFT, TitledBorder.TOP));
-		
-		
 		
 		/**
 		 * 	Setting control panel
 		 */
 		
-		//control.setBackground(Color.WHITE);
 		control.setBorder(BorderFactory.createTitledBorder(null, "WebServer control", TitledBorder.LEFT, TitledBorder.TOP));
 		control.setLayout(new GridLayout(2, 1));
 		
-		JButton button = new JButton("Start server");
-		button.setPreferredSize(new Dimension(1, 1));
+		startServer = new JButton("Start server");
+		startServer.setPreferredSize(new Dimension(1, 1));
+		startServer.setFocusPainted(false);
 		
-		JCheckBox maintenanceMode = new JCheckBox("Switch to maintenance mode");
+		maintenanceMode = new JCheckBox("Switch to maintenance mode");
 		maintenanceMode.setEnabled(false);
 		
-		control.add(button);
+		control.add(startServer);
 		control.add(maintenanceMode);
 		
 		
@@ -112,15 +129,69 @@ public class GUI extends JFrame {
 		 * 	Setting configuration panel
 		 */
 		
+		configuration.setBorder(BorderFactory.createTitledBorder(null, "WebServer configuration", TitledBorder.LEFT, TitledBorder.TOP));
+		configuration.setLayout(new GridLayout(3, 2, 0, 0));
+		
+		JLabel listeningPort = new JLabel("Server listening port");
+		listeningPort.setFont(listeningPort.getFont().deriveFont(listeningPort.getFont().getStyle() & ~Font.BOLD));
+		
+		JLabel rootDir = new JLabel("Web root directory");
+		rootDir.setFont(rootDir.getFont().deriveFont(rootDir.getFont().getStyle() & ~Font.BOLD));
+		
+		JLabel maintenanceDir = new JLabel("Maintanance directory");
+		maintenanceDir.setFont(maintenanceDir.getFont().deriveFont(maintenanceDir.getFont().getStyle() & ~Font.BOLD));
+	
+		tfPort = new JTextField(4);
+		tfRootDir = new JTextField(20);
+		tfMaintananceDir = new JTextField(20);
+		
+		browseRoot = new JButton("...");
+		browseMaintenance = new JButton("...");
+		ok = new JButton("Ok");
+		cancel = new JButton("Cancel");
+		
+		JPanel listenPanel = new JPanel(new BorderLayout(18, 0));
+		JPanel portPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		portPanel.add(tfPort);
+		
+		listenPanel.add(listeningPort, BorderLayout.WEST);
+		listenPanel.add(portPanel, BorderLayout.CENTER);
+		
+		
+		JPanel webPanel = new JPanel(new BorderLayout(30, 10));
+		JPanel webRootPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		webRootPanel.add(tfRootDir);
+		webRootPanel.add(browseRoot);
+		webRootPanel.add(ok);
+		
+		webPanel.add(rootDir, BorderLayout.WEST);
+		webPanel.add(webRootPanel, BorderLayout.CENTER);
+		
+		
+		JPanel mainPanel = new JPanel(new BorderLayout(10, 0));
+		JPanel maintenencePanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		maintenencePanel.add(tfMaintananceDir);
+		maintenencePanel.add(browseMaintenance);
+		maintenencePanel.add(cancel);
+		
+		mainPanel.add(maintenanceDir, BorderLayout.WEST);
+		mainPanel.add(maintenencePanel, BorderLayout.CENTER);
+		
+
+		
+		configuration.add(listenPanel);
+		configuration.add(webPanel);
+		configuration.add(mainPanel);
+		
 	}
 
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
             	new GUI();
             }
         });
 
-	}
+	}*/
 
 }
