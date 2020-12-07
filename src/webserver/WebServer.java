e9dc28afc54220dae72ebd1881f99485d48d2f65
AlexBosca <alex_241998@yahoo.com>
2020-12-07 12:34:50 +0200

    UI almost done

--- src/webserver/WebServer.java
+++ src/webserver/WebServer.java
@@ -1,6 +1,5 @@
 package webserver;
 
-//import java.awt.*;
 import java.net.*;
 import java.nio.file.Files;
 import java.nio.file.LinkOption;
@@ -18,9 +17,7 @@ import java.io.*;
 
 public class WebServer extends Thread {
 	
-	static final int PORT = 10008;
 	
-	//static final File ROOT = new File(".");
 	static final String[] DEFAULT_PAGES = {"index.html",
 											"index.htm",
 											"default.html"};
@@ -28,24 +25,27 @@ public class WebServer extends Thread {
 											"htdocs",
 											"public_html"};
 	
+	public static WebServer webServer;
 	
-	protected Socket clientSocket;
-	protected static ServerSocket serverSocket;
+	private Socket clientSocket;
+	private String directory;
+	private static ServerSocket serverSocket;
+	private static int port;
 	public static State state;
-	private static String directory;
-	private static boolean commandLineDir;
 	
-	protected enum State{
+	public static GUI gui;
+	
+	public enum State{
 		Stopped(true, "Run"),
 		Running(true, "Maintenance", "Stop"),
-		Maintenance(true, "Run");
+		Maintenance(true, "Run", "Stop");
 		
 		State(boolean expression, String... in){
 			inputs = Arrays.asList(in);
 			explicit = expression;
 		}
 		
-		State nextState(String input, State current) {
+		public State nextState(String input, State current) {
             if (inputs.contains(input)) {
                 return map.getOrDefault(input, current);
             }
@@ -60,56 +60,74 @@ public class WebServer extends Thread {
 			map.put("Run", State.Running);
 			map.put("Maintenance", State.Maintenance);
 			map.put("Stop", State.Stopped);
-			map.put("Run", State.Running);
 		}
 	}
 
 	public static void main(String[] args) throws IOException {
 		state = State.Stopped;
-		
-		if(args.length == 1) {
-			directory = args[0];
-			commandLineDir = true;
-		} else {
-			commandLineDir = false;
-		}
 
-		javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-            	new GUI();
-            }
-        });
+		gui = new GUI();
 		
-		/*try {
-			serverSocket = new ServerSocket(PORT);
-			state = state.nextState("Run", state);
-			System.out.println("Connection Socket Created");
-			try {
-				while (true) {
-					System.out.println("Waiting for Connection");
-					new WebServer(serverSocket.accept());
+		while(true) {
+				switch(state) {
+				case Maintenance:
+					serverSocket = new ServerSocket(port);
+					System.out.println("Connection Socket Created");
+					gui.setServerState(GUI.MAINTENANCE);
+					gui.setServerInfos(getAddress(), String.valueOf(getPort()));
+					try {
+						while (true) {
+							gui.maintenanceModeState(true);
+							gui.maintenanceFieldState(false);
+							gui.portFieldState(false);
+							gui.webRootFieldState(true);
+							System.out.println("Waiting for Connection");
+							webServer = new WebServer(serverSocket.accept(), gui.getMaintenanceDirectory().replace('\\', '/'));
+						}
+					} catch (IOException e) {
+						
+					}
+					break;
+				case Running:
+					serverSocket = new ServerSocket(port);
+					System.out.println("Connection Socket Created");
+					gui.setServerState(GUI.RUNNING);
+					gui.setServerInfos(getAddress(), String.valueOf(getPort()));
+					try {
+						while (true) {
+							gui.maintenanceModeState(true);
+							gui.maintenanceFieldState(true);
+							gui.portFieldState(false);
+							gui.webRootFieldState(false);
+							System.out.println("Waiting for Connection");
+							webServer = new WebServer(serverSocket.accept(), gui.getRootDirectory().replace('\\', '/'));
+						}
+					} catch (IOException e) {
+						if(state != State.Stopped && state != State.Maintenance) {
+							System.err.println("Accept failed.");
+							System.exit(1);
+						}
+					}
+					break;
+				case Stopped:
+					gui.maintenanceModeState(false);
+					gui.maintenanceFieldState(true);
+					gui.portFieldState(true);
+					gui.webRootFieldState(true);
+					break;
+				default:
+					break;
 				}
-			} catch (IOException e) {
-				System.err.println("Accept failed.");
-				System.exit(1);
-			}
-		} catch (IOException e) {
-			System.err.println("Could not listen on port: 10008.");
-			System.exit(1);
-		} finally {
-			try {
-				serverSocket.close();
-			} catch (IOException e) {
-				System.err.println("Could not close port: 10008.");
-				System.exit(1);
-			} finally {
-				state = State.Stopped;
-			}
-		}*/
+//			} catch(IOException e) {
+//				System.err.println("Could not listen on port: 10008.");
+//				System.exit(1);
+//			}
+		}
 	}
 
-	private WebServer(Socket clientSoc) {
-		clientSocket = clientSoc;
+	private WebServer(Socket clientSocket, String directory) {
+		this.clientSocket = clientSocket;
+		this.directory = directory;
 		start();
 	}
 
@@ -129,6 +147,12 @@ public class WebServer extends Thread {
 			String method = parse.nextToken().toUpperCase();
 			requestedFile = parse.nextToken().toLowerCase();
 			
+			/**
+			 *	Create two separate methods for loading web page and maintenance page
+			 */
+			
+			//String directory = gui.getRootDirectory().replace('\\', '/');
+			
 //			do {
 //				System.out.println("Server: " + inputLine);
 //				out.println(inputLine);
@@ -145,30 +169,30 @@ public class WebServer extends Thread {
 //			}
 			
 			if(method.equals("GET") || method.equals("HEAD")) {
-				//String root_path = "";
+				
 				if(requestedFile.endsWith("/")) {
-					if(WebServer.commandLineDir) {
-						if(filesFoldersExists(directory + "/" + "a.html")) {
-							requestedFile += /*directory + "/" +*/ "a.html";
-							//ROOT = new File("./" + directory);
-							//root_path += "./" + directory;
+						
+						if(filesFoldersExists(directory + "/" + DEFAULT_PAGES[0])) {
+							requestedFile += DEFAULT_PAGES[0];
 						}
-					} else {
-						if(filesFoldersExists(ROOT_DIRECTORIES[0] + "/" + DEFAULT_PAGES[0])) {
-							requestedFile += /*ROOT_DIRECTORIES[0] + "/" +*/ DEFAULT_PAGES[0];
-							//ROOT = new File("./" + ROOT_DIRECTORIES[0]);
-							//root_path = "./" + ROOT_DIRECTORIES[0];
+						else if(filesFoldersExists(directory + "/" + DEFAULT_PAGES[1])) {
+							requestedFile += DEFAULT_PAGES[1];
 						}
-					}
+						else if(filesFoldersExists(directory + "/" + DEFAULT_PAGES[2])) {
+							requestedFile += DEFAULT_PAGES[2];
+						}
+						else System.out.println("Nu exista");
 				}
 				
-				File root = (commandLineDir)?(new File("./" + directory)):(new File("./" + ROOT_DIRECTORIES[0]));
+				
+				File root = new File(directory);
 				
 				File file = new File(root, requestedFile);
 				int fileLength = (int) file.length();
 				String content = getContentType(requestedFile);
 			
 				if (method.equals("GET")) { // GET method so we return content
+					
 					byte[] fileData = readFileData(file, fileLength);
 					
 					// send HTTP Headers
@@ -224,4 +248,25 @@ public class WebServer extends Thread {
 		
 		return fileData;
 	}
+	
+	public static void closeServer() {
+		try {
+			serverSocket.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void setPort() {
+		port = gui.getPort();
+	}
+	
+	public static int getPort() {
+		return serverSocket.getLocalPort();
+	}
+	
+	public static String getAddress() {
+		return serverSocket.getInetAddress().getHostName();
+	}
+	
 }
--- src/webserver/gui/GUI.java
+++ src/webserver/gui/GUI.java
@@ -8,16 +8,20 @@ import java.awt.FlowLayout;
 import java.awt.Font;
 //import java.awt.Color;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.border.TitledBorder;
+import javax.swing.filechooser.FileSystemView;
 
 public class GUI extends JFrame {
 
@@ -26,20 +30,25 @@ public class GUI extends JFrame {
 	 */
 	private static final long serialVersionUID = 1L;
 	private static final String title = "VVS Web Server";
+	public static final String MAINTENANCE = "maintenance";
+	public static final String NOT_RUNNING = "not running";
+	public static final String RUNNING = "running...";
 	
 	private JPanel info, control, configuration;
 	private JPanel upperSection;
 	
-	private JLabel notRunning1;
-	private JLabel notRunning2;
-	private JLabel notRunning3;
+	private JLabel serverState;
+	private JLabel serverAddress;
+	private JLabel listeningPort;
 	
 	private JButton startServer;
 	private JCheckBox maintenanceMode;
 	
 	private JTextField tfPort;
 	private JTextField tfRootDir;
-	private JTextField tfMaintananceDir;
+	private JTextField tfMaintenanceDir;
+	
+	//private JFileChooser fileChooser; 
 	
 	private JButton browseRoot;
 	private JButton browseMaintenance;
@@ -77,17 +86,17 @@ public class GUI extends JFrame {
 		
 		info.setBorder(BorderFactory.createTitledBorder(null, "WebServer info", TitledBorder.LEFT, TitledBorder.TOP));
 		info.setLayout(new GridLayout(3, 2));
-		notRunning1 = new JLabel("not running");
-		notRunning1.setHorizontalAlignment(SwingConstants.RIGHT);
-		notRunning1.setFont(notRunning1.getFont().deriveFont(notRunning1.getFont().getStyle() & ~Font.BOLD));
+		serverState = new JLabel(NOT_RUNNING);
+		serverState.setHorizontalAlignment(SwingConstants.RIGHT);
+		serverState.setFont(serverState.getFont().deriveFont(serverState.getFont().getStyle() & ~Font.BOLD));
 		
-		notRunning2 = new JLabel("not running");
-		notRunning2.setHorizontalAlignment(SwingConstants.RIGHT);
-		notRunning2.setFont(notRunning2.getFont().deriveFont(notRunning2.getFont().getStyle() & ~Font.BOLD));
+		serverAddress = new JLabel(NOT_RUNNING);
+		serverAddress.setHorizontalAlignment(SwingConstants.RIGHT);
+		serverAddress.setFont(serverAddress.getFont().deriveFont(serverAddress.getFont().getStyle() & ~Font.BOLD));
 		
-		notRunning3 = new JLabel("not running");
-		notRunning3.setHorizontalAlignment(SwingConstants.RIGHT);
-		notRunning3.setFont(notRunning3.getFont().deriveFont(notRunning3.getFont().getStyle() & ~Font.BOLD));
+		listeningPort = new JLabel(NOT_RUNNING);
+		listeningPort.setHorizontalAlignment(SwingConstants.RIGHT);
+		listeningPort.setFont(listeningPort.getFont().deriveFont(listeningPort.getFont().getStyle() & ~Font.BOLD));
 		
 		JLabel status = new JLabel("Server status: ");
 		status.setFont(status.getFont().deriveFont(status.getFont().getStyle() & ~Font.BOLD));
@@ -100,11 +109,11 @@ public class GUI extends JFrame {
 		
 		
 		info.add(status);
-		info.add(notRunning1);
+		info.add(serverState);
 		info.add(address);
-		info.add(notRunning2);
+		info.add(serverAddress);
 		info.add(port);		
-		info.add(notRunning3);
+		info.add(listeningPort);
 		
 		
 		/**
@@ -117,9 +126,42 @@ public class GUI extends JFrame {
 		startServer = new JButton("Start server");
 		startServer.setPreferredSize(new Dimension(1, 1));
 		startServer.setFocusPainted(false);
+		startServer.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent event) {
+				if(WebServer.state == WebServer.State.Stopped) {
+					WebServer.state = WebServer.state.nextState("Run", WebServer.state);
+					startServer.setText("Stop Server");
+					WebServer.setPort();
+					
+				}
+				else {
+					WebServer.state = WebServer.state.nextState("Stop", WebServer.state);
+					startServer.setText("Start server");
+					WebServer.closeServer();
+					setServerState(NOT_RUNNING);
+					setServerInfos(NOT_RUNNING, NOT_RUNNING);
+				}
+				WebServer.gui.setTitle(title + " - " + "[" + WebServer.state + "]");
+			}
+		});
 		
 		maintenanceMode = new JCheckBox("Switch to maintenance mode");
 		maintenanceMode.setEnabled(false);
+		maintenanceMode.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent event) {
+				if(maintenanceMode.isSelected()) {
+					WebServer.state = WebServer.state.nextState("Maintenance", WebServer.state);
+				} else {
+					WebServer.state = WebServer.state.nextState("Run", WebServer.state);
+				}
+				WebServer.closeServer();
+				WebServer.gui.setTitle(title + " - " + "[" + WebServer.state + "]");
+				WebServer.setPort();
+			}
+			
+		});
 		
 		control.add(startServer);
 		control.add(maintenanceMode);
@@ -143,10 +185,40 @@ public class GUI extends JFrame {
 	
 		tfPort = new JTextField(4);
 		tfRootDir = new JTextField(20);
-		tfMaintananceDir = new JTextField(20);
+		tfMaintenanceDir = new JTextField(20);
 		
 		browseRoot = new JButton("...");
+		browseRoot.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser fileChooser = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+				fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+				int r = fileChooser.showSaveDialog(null);
+				
+				if(r == JFileChooser.APPROVE_OPTION) {
+					tfRootDir.setText(fileChooser.getSelectedFile().getAbsolutePath());
+				}
+			}
+			
+		});
+		
 		browseMaintenance = new JButton("...");
+		browseMaintenance.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JFileChooser fileChooser = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+				fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+				int r = fileChooser.showSaveDialog(null);
+				
+				if(r == JFileChooser.APPROVE_OPTION) {
+					tfMaintenanceDir.setText(fileChooser.getSelectedFile().getAbsolutePath());
+				}
+			}
+			
+		});
+		
 		ok = new JButton("Ok");
 		cancel = new JButton("Cancel");
 		
@@ -169,13 +241,13 @@ public class GUI extends JFrame {
 		
 		
 		JPanel mainPanel = new JPanel(new BorderLayout(10, 0));
-		JPanel maintenencePanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
-		maintenencePanel.add(tfMaintananceDir);
-		maintenencePanel.add(browseMaintenance);
-		maintenencePanel.add(cancel);
+		JPanel maintenancePanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		maintenancePanel.add(tfMaintenanceDir);
+		maintenancePanel.add(browseMaintenance);
+		maintenancePanel.add(cancel);
 		
 		mainPanel.add(maintenanceDir, BorderLayout.WEST);
-		mainPanel.add(maintenencePanel, BorderLayout.CENTER);
+		mainPanel.add(maintenancePanel, BorderLayout.CENTER);
 		
 
 		
@@ -185,13 +257,52 @@ public class GUI extends JFrame {
 		
 	}
 
-	/*public static void main(String[] args) {
-		javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-            	new GUI();
-            }
-        });
-
-	}*/
+	public void maintenanceModeState(boolean state) {
+		this.maintenanceMode.setEnabled(state);
+	}
+	
+	public void portFieldState(boolean state) {
+		this.tfPort.setEnabled(state);
+	}
+	
+	public void webRootFieldState(boolean state) {
+		this.tfRootDir.setEnabled(state);
+		this.browseRoot.setEnabled(state);
+	}
+	
+	public void maintenanceFieldState(boolean state) {
+		this.tfMaintenanceDir.setEnabled(state);
+		this.browseMaintenance.setEnabled(state);
+	}
+	
+	public String getRootDirectory() {
+		return tfRootDir.getText().trim();
+	}
+	
+	public String getMaintenanceDirectory() {
+		return tfMaintenanceDir.getText().trim();
+	}
+	
+	public int getPort() {
+		return Integer.parseInt(tfPort.getText().trim());
+	}
+	
+	public void setServerInfos(String serverAddress, String serverPort) {
+		this.serverAddress.setText(serverAddress);
+		this.listeningPort.setText(serverPort);
+	}
+	
+	public void setServerState(String state) {
+		this.serverState.setText(state);
+	}
+	
+//	public static void main(String[] args) {
+//		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+//            public void run() {
+//            	new GUI();
+//            }
+//        });
+//
+//	}
 
 }
