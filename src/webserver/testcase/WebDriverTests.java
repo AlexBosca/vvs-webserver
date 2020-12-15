7e2ee33f35fa636d896cdb34ed307e654f6b8a3a
AlexBosca <alex_241998@yahoo.com>
2020-12-15 14:21:28 +0200

    test for one link

--- src/webserver/testcase/WebDriverTests.java
+++ src/webserver/testcase/WebDriverTests.java
@@ -1,5 +1,47 @@
 package webserver.testcase;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import webserver.WebServer;
+
+
 public class WebDriverTests {
 	
+	WebDriver driver;
+	
+	@BeforeClass
+	public static void setUp() {
+		new Thread(() -> {
+			System.out.println("Started Test!!");
+			WebServer.setUpGUI();
+			WebServer.gui.setMaintenanceField("D:\\VVS\\webserver\\www\\mantenance");
+			WebServer.gui.setRootDirectory("D:\\VVS\\webserver\\www\\mySite");
+			WebServer.gui.setPort("10008");
+			WebServer.gui.clickStartButton();
+			WebServer.startWebServer();
+		}).start();
+	}
+	
+	@Test
+	public void testMySite() {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\alex_\\Downloads\\chromedriver_win32\\chromedriver.exe");
+		
+		driver = new ChromeDriver();
+		
+		driver.navigate().to("http://localhost:10008/");
+		
+		driver.findElement(By.name("button1")).click();
+	}
+	
+	@After
+	public void after() {
+		driver.quit();
+		System.out.println("Test finished!!");
+	}
 }
