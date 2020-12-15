63fc575cdbb824757f14b41de9a5e03aed685e41
AlexBosca <alex_241998@yahoo.com>
2020-12-15 14:24:18 +0200

    WebServer updated

--- src/webserver/WebServer.java
+++ src/webserver/WebServer.java
@@ -64,73 +64,69 @@ public class WebServer extends Thread {
 	}
 
 	public static void main(String[] args) {
-		try {
-			startWebServer();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		setUpGUI();
+		startWebServer();
 	}
 	
-	public static void startWebServer() throws IOException {
-		state = State.Stopped;
-
-		gui = new GUI();
-		
-		while(true) {
-				switch(state) {
-				case Maintenance:
-					serverSocket = new ServerSocket(port);
-					System.out.println("Connection Socket Created");
-					gui.setServerState(GUI.MAINTENANCE);
-					gui.setServerInfos(getAddress(), String.valueOf(getPort()));
-					try {
-						while (true) {
-							gui.maintenanceModeState(true);
-							gui.maintenanceFieldState(false);
-							gui.portFieldState(false);
-							gui.webRootFieldState(true);
-							System.out.println("Waiting for Connection");
-							webServer = new WebServer(serverSocket.accept(), gui.getMaintenanceDirectory().replace('\\', '/'));
+	public static void startWebServer() {
+		try {
+			while(true) {
+					switch(state) {
+					case Maintenance:
+						serverSocket = new ServerSocket(port);
+						System.out.println("Connection Socket Created");
+						gui.setServerState(GUI.MAINTENANCE);
+						gui.setServerInfos(getAddress(), String.valueOf(getPort()));
+						try {
+							while (true) {
+								gui.maintenanceModeState(true);
+								gui.maintenanceFieldState(false);
+								gui.portFieldState(false);
+								gui.webRootFieldState(true);
+								System.out.println("Waiting for Connection");
+								webServer = new WebServer(serverSocket.accept(), gui.getMaintenanceDirectory().replace('\\', '/'));
+							}
+						} catch (IOException e) {
+							if(state != State.Stopped && state != State.Running) {
+								System.err.println("Accept failed.");
+								System.exit(1);
+							}
 						}
-					} catch (IOException e) {
-						if(state != State.Stopped && state != State.Running) {
-							System.err.println("Accept failed.");
-							System.exit(1);
+						break;
+					case Running:
+						serverSocket = new ServerSocket(port);
+						System.out.println("Connection Socket Created");
+						gui.setServerState(GUI.RUNNING);
+						gui.setServerInfos(getAddress(), String.valueOf(getPort()));
+						try {
+							while (true) {
+								gui.maintenanceModeState(true);
+								gui.maintenanceFieldState(true);
+								gui.portFieldState(false);
+								gui.webRootFieldState(false);
+								System.out.println("Waiting for Connection");
+								webServer = new WebServer(serverSocket.accept(), gui.getRootDirectory().replace('\\', '/'));
+							}
+						} catch (IOException e) {
+							if(state != State.Stopped && state != State.Maintenance) {
+								System.err.println("Accept failed.");
+								System.exit(1);
+							}
 						}
+						break;
+					case Stopped:
+						gui.maintenanceModeState(false);
+						gui.maintenanceFieldState(true);
+						gui.portFieldState(true);
+						gui.webRootFieldState(true);
+						gui.deselectCheckBox();
+						break;
+					default:
+						break;
 					}
-					break;
-				case Running:
-					serverSocket = new ServerSocket(port);
-					System.out.println("Connection Socket Created");
-					gui.setServerState(GUI.RUNNING);
-					gui.setServerInfos(getAddress(), String.valueOf(getPort()));
-					try {
-						while (true) {
-							gui.maintenanceModeState(true);
-							gui.maintenanceFieldState(true);
-							gui.portFieldState(false);
-							gui.webRootFieldState(false);
-							System.out.println("Waiting for Connection");
-							webServer = new WebServer(serverSocket.accept(), gui.getRootDirectory().replace('\\', '/'));
-						}
-					} catch (IOException e) {
-						if(state != State.Stopped && state != State.Maintenance) {
-							System.err.println("Accept failed.");
-							System.exit(1);
-						}
-					}
-					break;
-				case Stopped:
-					gui.maintenanceModeState(false);
-					gui.maintenanceFieldState(true);
-					gui.portFieldState(true);
-					gui.webRootFieldState(true);
-					gui.deselectCheckBox();
-					break;
-				default:
-					break;
-				}
+			}
+		} catch(IOException e) {
+			e.printStackTrace();
 		}
 	}
 
@@ -207,6 +203,11 @@ public class WebServer extends Thread {
 			e.printStackTrace();
 			System.exit(1);
 		}
+	}
+	
+	public static void setUpGUI() {
+		state = State.Stopped;
+		gui = new GUI();
 	}
 	
 	public static boolean filesFoldersExists(String path) {
