5997cf31bb202ea2d124524e5f9918357747805d
AlexBosca <alex_241998@yahoo.com>
2020-12-16 17:16:52 +0200

    test for login page

--- src/webserver/testcase/WebDriverTests.java
+++ src/webserver/testcase/WebDriverTests.java
@@ -1,11 +1,17 @@
 package webserver.testcase;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import webserver.WebServer;
@@ -16,7 +22,7 @@ public class WebDriverTests {
 	WebDriver driver;
 	
 	@BeforeClass
-	public static void setUp() {
+	public static void setUpClass() {
 		new Thread(() -> {
 			System.out.println("Started Test!!");
 			WebServer.setUpGUI();
@@ -28,15 +34,86 @@ public class WebDriverTests {
 		}).start();
 	}
 	
-	@Test
-	public void testMySite() {
+	@Before
+	public void setUp() {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\alex_\\Downloads\\chromedriver_win32\\chromedriver.exe");
 		
 		driver = new ChromeDriver();
-		
+	}
+	
+	@Ignore@Test
+	public void testTitle() {
 		driver.navigate().to("http://localhost:10008/");
 		
-		driver.findElement(By.name("button1")).click();
+		String actualTitle = driver.getTitle();
+		String expectedTitle = "My VVS Web Page";
+		
+		assertEquals(expectedTitle, actualTitle);
+	}
+	
+	@Ignore@Test
+	public void testLoginUsernameField() {
+		String expectedUsername = "some_Us3rN4me";
+		String actualUsername;
+		
+		accessLoginPage();
+		WebElement username = driver.findElement(By.id("username"));
+		username.sendKeys(expectedUsername);
+		
+		actualUsername = username.getAttribute("value");
+		
+		assertEquals(expectedUsername, actualUsername);
+	}
+	
+	@Ignore@Test
+	public void testLoginPasswordField() {
+		String expectedPassword = "P4ssw0rdd";
+		String actualPassword;
+		
+		accessLoginPage();
+		WebElement password = driver.findElement(By.className("password"));
+		password.sendKeys(expectedPassword);
+		
+		actualPassword = password.getAttribute("value");
+		
+		assertEquals(expectedPassword, actualPassword);
+			
+	}
+	
+	@Ignore@Test
+	public void testLoginButton() {
+		accessLoginPage();
+		driver.findElement(By.id("login")).click();
+		assertEquals("http://localhost:10008/index.html", driver.getCurrentUrl());
+	}
+	
+	@Ignore@Test
+	public void testPasswordForgot() {
+		accessLoginPage();
+		driver.findElement(By.name("password")).click();
+		assertEquals("http://localhost:10008/Button1/Button1.html#", driver.getCurrentUrl());
+	}
+	
+	@Ignore@Test
+	public void testRememberMeSelected() {
+		accessLoginPage();
+		assertTrue(Boolean.parseBoolean(driver.findElement(By.id("remember")).getAttribute("checked")));
+	}
+	
+	@Test
+	public void testRememberMeUnselected() {
+		accessLoginPage();
+		WebElement checkBox =  driver.findElement(By.id("remember"));
+		checkBox.click();
+		assertFalse(Boolean.parseBoolean(checkBox.getAttribute("checked")));
+	}
+	
+	public void accessLoginPage() {
+		driver.navigate().to("http://localhost:10008/Button1/Button1.html");
+	}
+	
+	public void accessRegisterPage() {
+		driver.navigate().to("http://localhost:10008/Button2/Button2.html");
 	}
 	
 	@After
